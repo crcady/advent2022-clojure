d9a358c5d4a965d41718baf467b75e579857a38c
Camdon Cady <camdon.cady@gmail.com>
2024-10-30 21:15:53 +0000

    Day 9b

--- src/day09/core.clj
+++ src/day09/core.clj
@@ -11,12 +11,12 @@
        (recur (concat so-far (repeat (parse-long how-many) direction)) (rest remaining))))))
 
 (defn move-head
-  [posns move]
+  [point move]
   (case move
-    "U" (assoc posns :hy (inc (:hy posns)))
-    "D" (assoc posns :hy (dec (:hy posns)))
-    "R" (assoc posns :hx (inc (:hx posns)))
-    "L" (assoc posns :hx (dec (:hx posns)))))
+    "U" (assoc point :y (inc (:y point)))
+    "D" (assoc point :y (dec (:y point)))
+    "R" (assoc point :x (inc (:x point)))
+    "L" (assoc point :x (dec (:x point)))))
 
 (defn clamp
   [lower-bound upper-bound input]
@@ -25,24 +25,35 @@
 (def clamp1 (partial clamp -1 1))
 
 (defn move-tail
-  [posns]
-  (let [hx (:hx posns) hy (:hy posns) tx (:tx posns) ty (:ty posns)
+  [head tail]
+  (let [hx (:x head) hy (:y head) tx (:x tail) ty (:y tail)
         dx (- hx tx) dy (- hy ty)]
     (if (or (= 0 dx) (= 0 dy)) ; Either saame row or same column
-      (assoc posns :tx (+ tx (int (/ dx 2))) :ty (+ ty (int (/ dy 2))))
-      (if (= (+ (abs dx) (abs dy)) 3) ; Diagonal and manhattan distance is 3
-        (assoc posns :tx (+ tx (clamp1 dx)) :ty (+ ty (clamp1 dy)))
-        posns))))
+      (assoc tail :x (+ tx (int (/ dx 2))) :y (+ ty (int (/ dy 2))))
+      (if (>= (+ (abs dx) (abs dy)) 3) ; Diagonal and manhattan distance is 3 or more
+        (assoc tail :x (+ tx (clamp1 dx)) :y (+ ty (clamp1 dy)))
+        tail))))
 
-(def initial-position {:hx 0 :hy 0 :tx 0 :ty 0})
+(defn follow-tail
+  [moved remaining]
+  (if (empty? remaining)
+    moved
+    (recur (cons (move-tail (first moved) (first remaining)) moved) (rest remaining))))
+
+(defn apply-move
+  [posns move]
+  (let [remaining (reverse posns)]
+    (follow-tail (list (move-head (first remaining) move)) (rest remaining))))
+
+(defn initial-position
+  [n]
+  (repeat n {:x 0 :y 0}))
 
 (defn moves-to-positions
-  ([moves]
-   (moves-to-positions (list initial-position) moves))
-  ([positions moves]
+  ([positions moves] ; positions is a list of lists of maps
    (if (empty? moves)
      positions
-     (recur (cons (move-tail (move-head (first positions) (first moves))) positions) (rest moves)))))
+     (recur (cons (apply-move (first positions) (first moves)) positions) (rest moves)))))
 
 (defn load-as-moves
   [fname]
@@ -50,4 +61,8 @@
 
 (defn solve-first
   [fname]
-  (count (set (map (fn [x] [(:tx x) (:ty x)]) (moves-to-positions (load-as-moves fname))))))
+  (count (set (map (fn [x] (first x)) (moves-to-positions (list (initial-position 2)) (load-as-moves fname))))))
+
+(defn solve-second
+  [fname]
+  (count (set (map (fn [x] (first x)) (moves-to-positions (list (initial-position 10)) (load-as-moves fname))))))
